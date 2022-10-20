a8fc7731b234d32a4f9a9d0b38ef9c8284d9efbc
Akshaymohite383 <113896306+Akshaymohite383@users.noreply.github.com>
2022-10-20 14:18:21 +0530

    ioc4

--- App.java
+++ App.java
@@ -1,15 +1,14 @@
-package com.yash.springIOCAssignment3;
+package com.yash.springIOCAssignment4;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App 
 {
-    public static void main( String[] args ) {
-    
-    	ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-    	ConstructorMessage msg = (ConstructorMessage) context.getBean("message");
-    	msg.display();
-    
+    public static void main( String[] args )
+    {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        SetterMessage msg= (SetterMessage) context.getBean("message1");
+        msg.display();
     }
 }
